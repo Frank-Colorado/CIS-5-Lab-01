647790b6f37e53e385f78ba5e8c63d6c12c5c083
Frank Colorado <chawnkiee@gmail.com>
2026-01-12 17:19:19 -0800

    Commented main.cpp file
    Added comments to my code explaining my reasoning behind each line.

--- Lab_01_Hello_World/main.cpp
+++ Lab_01_Hello_World/main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>	
 
+// Delclared namespaces for std with cout and endl as demonstrated in your lab 01 video
 using std::cout;
 using std::endl;
 
+// Main function to execute the program
 int main() {
 
+	// Used cout to print Hello World to the console
 	cout << "Hello World!" << endl;
 
+	// returned 0 because the function is of type int and expecting an integer return value
 	return 0;
 
 };
